40bdac82d1879cab8247632025907d67f04ec291
Manish Hedaoo <manishhedaoo2468@gmail.com>
2020-05-04 20:12:13 -0700

    HW_Project

---
+++ src/test/java/HelloWorld_test.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorld_test {
+	private HelloWorld_test h;
+	@Test
+	public void test() {
+	
+		assertEquals(h.getMessage(),"Hello World!");
+	
+	
+	}
+	private Object getMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
